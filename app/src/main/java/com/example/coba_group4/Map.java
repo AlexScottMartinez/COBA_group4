4d12c1623909598265b2a136bdd2eaac99f64347
BigText <MHNMmmmmTacos@hotmail.com>
2021-04-23 20:01:25 -0500

    Initial implementation of map, needs occurrences to be updated.  Added API key, Added map_api.xml, PinActivity.java, activity_pin.xml, added request for location permission to manifest imported "com.google.android.gms:play-services-maps:17.0.0"

--- app/src/main/java/com/example/coba_group4/Map.java
+++ app/src/main/java/com/example/coba_group4/Map.java
@@ -1,15 +1,53 @@
 package com.example.coba_group4;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class Map extends AppCompatActivity
+import com.example.coba_group4.database.OccurrenceDB;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Map extends FragmentActivity implements OnMapReadyCallback
 {
+    private OccurrenceDB occurenceDB;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng arlington = new LatLng(32.7357, -97.1081);
+        mMap.addMarker(new MarkerOptions().position(arlington).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(arlington));
+        // enable clicking on pins
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String markertitle = marker.getTitle();
+                Intent i = new Intent(Map.this, PinActivity.class);
+                i.putExtra("title", markertitle);
+                startActivity(i);
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/coba_group4/PinActivity.java
@@ -0,0 +1,13 @@
+package com.example.coba_group4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin);
+    }
+}
