ec1067efd62510820b613a9a016601d36ed466f3
BigText <MHNMmmmmTacos@hotmail.com>
2021-04-26 00:38:39 -0500

    Pins use relative data and are generated based one reports now, manifest includes pin activity, on back press map returns to main menu now

--- app/src/main/java/com/example/coba_group4/Map.java
+++ app/src/main/java/com/example/coba_group4/Map.java
@@ -1,10 +1,14 @@
 package com.example.coba_group4;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.coba_group4.database.OccurrenceDB;
+import com.example.coba_group4.occurence.Occurrence;
+import com.google.android.gms.common.server.converter.StringToIntConverter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,28 +17,54 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map extends FragmentActivity implements OnMapReadyCallback
 {
-    private OccurrenceDB occurenceDB;
+    ArrayList<Occurrence> occurrences;
+    OccurrenceDB occurenceDB = new OccurrenceDB();
     private GoogleMap mMap;
+    @Override
+    public void onBackPressed() {
+        finish();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        occurrences = new ArrayList<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync((OnMapReadyCallback) this);
+        occurrences = occurenceDB.getAllOccurrences();
     }
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
-        mMap = googleMap;
-
+       List<Address> addressList = null;
+       mMap = googleMap;
+       Geocoder geocoder = new Geocoder(this);
         // Add a marker in Sydney and move the camera
-        LatLng arlington = new LatLng(32.7357, -97.1081);
-        mMap.addMarker(new MarkerOptions().position(arlington).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(arlington));
+        for(int i = 0 ; i < occurrences.size() ; i ++ )
+        {
+            if(occurrences.get(i).getAddress() != null)
+            try{
+                addressList = geocoder.getFromLocationName(occurrences.get(i).getAddress() + ", " + occurrences.get(i).getCity() + ", " +
+                        occurrences.get(i).getState(), 1);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            if( addressList != null) {
+                Address address = addressList.get(0);
+                LatLng Latlng = new LatLng(address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(Latlng).title(String.valueOf(i)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(Latlng));
+            }
+        }
         // enable clicking on pins
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
@@ -42,6 +72,10 @@ public class Map extends FragmentActivity implements OnMapReadyCallback
                 String markertitle = marker.getTitle();
                 Intent i = new Intent(Map.this, PinActivity.class);
                 i.putExtra("title", markertitle);
+                i.putExtra("location",occurrences.get(Integer.parseInt(markertitle)).getAddress() + ", " + occurrences.get(Integer.parseInt(markertitle)).getCity() + ", " +
+                        occurrences.get(Integer.parseInt(markertitle)).getState() );
+                i.putExtra("Time",occurrences.get(Integer.parseInt(markertitle)).getSubmittedTime()  );
+                i.putExtra("Description",occurrences.get(Integer.parseInt(markertitle)).getDescription()  );
                 startActivity(i);
                 return false;
             }
